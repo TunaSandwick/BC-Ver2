ef7267c5c399681948fbbede114bd149e36bb951
MTuan <mtuannscool240799@gmail.com>
2024-10-04 14:58:30 +0700

    add form api

--- js/views/view.contact.js
+++ js/views/view.contact.js
@@ -1,16 +1,14 @@
-
-
-(function($) {
+(function ($) {
 
 	'use strict';
 
 	/*
 	Custom Rules
 	*/
-	
+
 	// No White Space
-	$.validator.addMethod("noSpace", function(value, element) {
-		if( $(element).attr('required') ) {
+	$.validator.addMethod("noSpace", function (value, element) {
+		if ($(element).attr('required')) {
 			return value.search(/^(?! *$)[^]+$/) == 0;
 		}
 
@@ -21,30 +19,30 @@
 	Assign Custom Rules on Fields
 	*/
 	$.validator.addClassRules({
-	    'form-control': {
-	        noSpace: true
-	    }
+		'form-control': {
+			noSpace: true
+		}
 	});
 
 	/*
 	Contact Form: Basic
 	*/
-	$('.contact-form').each(function(){
+	$('.contact-form').each(function () {
 		$(this).validate({
-			errorPlacement: function(error, element) {
-				if(element.attr('type') == 'radio' || element.attr('type') == 'checkbox') {
+			errorPlacement: function (error, element) {
+				if (element.attr('type') == 'radio' || element.attr('type') == 'checkbox') {
 					error.appendTo(element.closest('.form-group'));
-				} else if( element.is('select') && element.closest('.custom-select-1') ) {
+				} else if (element.is('select') && element.closest('.custom-select-1')) {
 					error.appendTo(element.closest('.form-group'));
 				} else {
-					if( element.closest('.form-group').length ) {
+					if (element.closest('.form-group').length) {
 						error.appendTo(element.closest('.form-group'));
 					} else {
 						error.insertAfter(element);
 					}
 				}
 			},
-			submitHandler: function(form) {
+			submitHandler: function (form) {
 
 				var $form = $(form),
 					$messageSuccess = $form.find('.contact-form-success'),
@@ -53,38 +51,49 @@
 					$errorMessage = $form.find('.mail-error-message'),
 					submitButtonText = $submitButton.val();
 
-				$submitButton.val( $submitButton.data('loading-text') ? $submitButton.data('loading-text') : 'Loading...' ).attr('disabled', true);
+				$submitButton.val($submitButton.data('loading-text') ? $submitButton.data('loading-text') : 'Loading...').attr('disabled', true);
 
 				// Fields Data
 				var formData = $form.serializeArray(),
 					data = {};
 
-				$(formData).each(function(index, obj){
-					if( data[obj.name] ) {
-						data[obj.name] = data[obj.name] + ', ' + obj.value;						
+				$(formData).each(function (index, obj) {
+					if (data[obj.name]) {
+						data[obj.name] = data[obj.name] + ', ' + obj.value;
 					} else {
 						data[obj.name] = obj.value;
 					}
 				});
 
 				// Google Recaptcha v2
-				if( data["g-recaptcha-response"] != undefined ) {
+				if (data["g-recaptcha-response"] != undefined) {
 					data["g-recaptcha-response"] = $form.find('#g-recaptcha-response').val();
+					if (data["g-recaptcha-response"].length === 0) {
+						$(".contact-form-error").addClass("d-block").html("Vui lòng xác minh reCAPTCHA.");
+						$messageSuccess.addClass('d-none');
+						$messageError.removeClass('d-none');
+						$submitButton.val(submitButtonText).attr('disabled', false);
+						return
+					}
+				}
+
+				// Set action
+				var action = $(this).attr("action");
+				if (!action) {
+					action =
+						"https://script.google.com/macros/s/AKfycbxRtnFA9Kk8A9xv0GmUqWVLLjhfqD3WUjT-1ebzVE4wVsEKY4DM9Wzl7rzY5f2QXU9xTA/exec";
 				}
 
 				// Ajax Submit
 				$.ajax({
 					type: 'POST',
-					url: $form.attr('action'),
+					url: action,
 					data: data
-				}).always(function(data, textStatus, jqXHR) {
+				}).always(function (data, textStatus, jqXHR) {
 
 					$errorMessage.empty().hide();
 
-					if (data.response == 'success') {
-
-						// Uncomment the code below to redirect for a thank you page
-						// self.location = 'thank-you.html';
+					if (data.result == 'success') {
 
 						$messageSuccess.removeClass('d-none');
 						$messageError.addClass('d-none');
@@ -107,167 +116,15 @@
 
 						$form.find('.form-control').removeClass('error');
 
-						$submitButton.val( submitButtonText ).attr('disabled', false);
-						
+						$submitButton.val(submitButtonText).attr('disabled', false);
+
 						return;
 
-					} else if (data.response == 'error' && typeof data.errorMessage !== 'undefined') {
-						$errorMessage.html(data.errorMessage).show();
 					} else {
-						$errorMessage.html(data.responseText).show();
-					}
-
-					$messageError.removeClass('d-none');
-					$messageSuccess.addClass('d-none');
-
-					if (($messageError.offset().top - 80) < $(window).scrollTop()) {
-						$('html, body').animate({
-							scrollTop: $messageError.offset().top - 80
-						}, 300);
-					}
-
-					$form.find('.has-success')
-						.removeClass('has-success');
-						
-					$submitButton.val( submitButtonText ).attr('disabled', false);
-
-				});
-			}
-		});
-	});
-
-	/*
-	Contact Form: Advanced
-	*/
-	$('#contactFormAdvanced').validate({
-		onkeyup: false,
-		onclick: false,
-		onfocusout: false,
-		rules: {
-			'captcha': {
-				captcha: true
-			},
-			'checkboxes[]': {
-				required: true
-			},
-			'radios': {
-				required: true
-			}
-		},
-		errorPlacement: function(error, element) {
-			if(element.attr('type') == 'radio' || element.attr('type') == 'checkbox') {
-				error.appendTo(element.closest('.form-group'));
-			} else if( element.is('select') && element.closest('.custom-select-1') ) {
-				error.appendTo(element.closest('.form-group'));
-			} else {
-				error.insertAfter(element);
-			}
-		}
-	});
-
-	/*
-	Contact Form: reCaptcha v3
-	*/
-	$('.contact-form-recaptcha-v3').each(function(){
-		$(this).validate({
-			errorPlacement: function(error, element) {
-				if(element.attr('type') == 'radio' || element.attr('type') == 'checkbox') {
-					error.appendTo(element.closest('.form-group'));
-				} else if( element.is('select') && element.closest('.custom-select-1') ) {
-					error.appendTo(element.closest('.form-group'));
-				} else {
-					error.insertAfter(element);
-				}
-			},
-			submitHandler: function(form) {
-
-				var $form = $(form),
-					$messageSuccess = $form.find('.contact-form-success'),
-					$messageError = $form.find('.contact-form-error'),
-					$submitButton = $(this.submitButton),
-					$errorMessage = $form.find('.mail-error-message'),
-					submitButtonText = $submitButton.val();
-
-				$submitButton.val( $submitButton.data('loading-text') ? $submitButton.data('loading-text') : 'Loading...' ).attr('disabled', true);
-
-				var recaptchaSrcURL = $('#google-recaptcha-v3').attr('src'),
-					newURL          = new URL(recaptchaSrcURL),
-					site_key        = newURL.searchParams.get("render");
-
-				grecaptcha.execute(site_key, {action: 'contact_us'}).then(function(token) {
-
-					// Fields Data
-					var formData = $form.serializeArray(),
-						data = {};
-
-					$(formData).each(function(index, obj){
-					    data[obj.name] = obj.value;
-					});
-
-					// Recaptcha v3 Token
-					data["g-recaptcha-response"] = token;
-
-					// Ajax Submit
-					$.ajax({
-						type: 'POST',
-						url: $form.attr('action'),
-						data: data
-					}).always(function(data, textStatus, jqXHR) {
-
-						$errorMessage.empty().hide();
-
-						if (data.response == 'success') {
-
-							// Uncomment the code below to redirect for a thank you page
-							// self.location = 'thank-you.html';
-
-							$messageSuccess.removeClass('d-none');
-							$messageError.addClass('d-none');
-
-							// Reset Form
-							$form.find('.form-control')
-								.val('')
-								.blur()
-								.parent()
-								.removeClass('has-success')
-								.removeClass('has-danger')
-								.find('label.error')
-								.remove();
-
-							if (($messageSuccess.offset().top - 80) < $(window).scrollTop()) {
-								$('html, body').animate({
-									scrollTop: $messageSuccess.offset().top - 80
-								}, 300);
-							}
-
-							$form.find('.form-control').removeClass('error');
-
-							$submitButton.val( submitButtonText ).attr('disabled', false);
-							
-							return;
-
-						} else if (data.response == 'error' && typeof data.errorMessage !== 'undefined') {
-							$errorMessage.html(data.errorMessage).show();
-						} else {
-							$errorMessage.html(data.responseText).show();
-						}
-
-						$messageError.removeClass('d-none');
+						$(".contact-form-error").addClass("d-block").html("Đã có lỗi xảy ra! Vui lòng thử lại sau.");
 						$messageSuccess.addClass('d-none');
-
-						if (($messageError.offset().top - 80) < $(window).scrollTop()) {
-							$('html, body').animate({
-								scrollTop: $messageError.offset().top - 80
-							}, 300);
-						}
-
-						$form.find('.has-success')
-							.removeClass('has-success');
-							
-						$submitButton.val( submitButtonText ).attr('disabled', false);
-
-					});
-
+						$messageError.removeClass('d-none');
+					}
 				});
 			}
 		});
